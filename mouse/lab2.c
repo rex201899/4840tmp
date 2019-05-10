a20d47d38d8c3e134cc5d6c528147a9b7ecc3a3b
zhongtai <zr2208@columbia.edu>
2019-05-10 15:32:46 -0400

    d

--- mouse/lab2.c
+++ mouse/lab2.c
@@ -12,7 +12,7 @@ int main(void)
 	struct mouse_info *mouse0;
 	init_mouse();
 	read_mouse(mouse0);
-	printf("position of x, y are: %d %d; left click is %d\n",mouse0.x,mouse0.y,mouse0.button);
+	printf("position of x, y are: %d %d; left click is %d\n",&mouse0->x,&mouse0->y,&mouse0->button);
       
 	return 0;
 	
