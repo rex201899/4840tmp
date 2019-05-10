3e3a1e2f54eb5b42e4cd355283d46cf77a5cfbfb
zhongtai <zr2208@columbia.edu>
2019-05-10 16:07:55 -0400

    d

--- mouse/lab2.c
+++ mouse/lab2.c
@@ -13,12 +13,18 @@ int main(void)
 	struct mouse_info mouse0;
 	// struct mouse_info mouse0;
 	init_mouse();
-	read_mouse(&mouse0);
 
+	for(;;){
+		read_mouse(&mouse0);
+
+		printf("position of x, y are: %d %d; left click is %d\n",mouse0.x,mouse0.y,mouse0.button);
+	}
 	// read_mouse(&mouse0);
-	// printf("position of x, y are: %d %d; left click is %d\n",mouse0.x,&mouse0->y,&mouse0->button);
-    // printf("position of x is %d ", mouse0.x);
-    printf("position of x, y are: %d %d; left click is %d\n",mouse0.x,mouse0.y,mouse0.button);
+
+	// // read_mouse(&mouse0);
+	// // printf("position of x, y are: %d %d; left click is %d\n",mouse0.x,&mouse0->y,&mouse0->button);
+ //    // printf("position of x is %d ", mouse0.x);
+ //    printf("position of x, y are: %d %d; left click is %d\n",mouse0.x,mouse0.y,mouse0.button);
         
 	return 0;
 	
--- mouse/read_mouse.c
+++ mouse/read_mouse.c
@@ -52,40 +52,37 @@ void read_mouse(struct mouse_info *ptr)
   // }
     
 
-  for (;;) 
-  {
-    libusb_interrupt_transfer(mouse, endpoint_address,
-            (unsigned char *) &packet, sizeof(packet),
-            &transferred, 0);
-    // printf("%d\n", flg1);
+  libusb_interrupt_transfer(mouse, endpoint_address,
+          (unsigned char *) &packet, sizeof(packet),
+          &transferred, 0);
+  // printf("%d\n", flg1);
 
-    if (transferred == sizeof(packet)) {
-      if (packet.pos_x > 0x88) {
-        numx = -(0xFF - packet.pos_x + 1);
-      }
-      else { numx = packet.pos_x;}
+  if (transferred == sizeof(packet)) {
+    if (packet.pos_x > 0x88) {
+      numx = -(0xFF - packet.pos_x + 1);
+    }
+    else { numx = packet.pos_x;}
 
-      if (packet.pos_y > 0x88) {
-        numy = -(0xFF - packet.pos_y + 1);
-      }
-      else { numy = packet.pos_y;}
+    if (packet.pos_y > 0x88) {
+      numy = -(0xFF - packet.pos_y + 1);
+    }
+    else { numy = packet.pos_y;}
 
-      if (px < 1) { px = 1;}
-      else if (px > 0 && px < 640) { px = px + numx; }
-      else if (px > 639) { px = 639;}
-      else {px = 320;}
+    if (px < 1) { px = 1;}
+    else if (px > 0 && px < 640) { px = px + numx; }
+    else if (px > 639) { px = 639;}
+    else {px = 320;}
 
-      if (py < 1) { py = 1;}
-      else if (py > 0 && py < 480) { py = py + numy; }
-      else if (py > 479) { py = 479;}
-      else {py = 240;}
+    if (py < 1) { py = 1;}
+    else if (py > 0 && py < 480) { py = py + numy; }
+    else if (py > 479) { py = 479;}
+    else {py = 240;}
 
-      modifierss = packet.modifiers;
-      ptr->x=px;
-      ptr->y=py;
-      ptr->button=modifierss;
-      // printf("position of x, y are: %d %d; left click is %d\n",px,py,modifierss);
-      }
+    modifierss = packet.modifiers;
+    ptr->x=px;
+    ptr->y=py;
+    ptr->button=modifierss;
+    printf("position of x, y are: %d %d; left click is %d\n",px,py,modifierss);
     }
   return 0;
 }
